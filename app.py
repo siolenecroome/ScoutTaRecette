c4d905f85258c9d3df1f739f798b9f2c467ea499
siolenecroome <siolene.croome@epfl.ch>
2025-11-12 19:28:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -129,4 +129,8 @@ def valider():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT dynamically
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+
